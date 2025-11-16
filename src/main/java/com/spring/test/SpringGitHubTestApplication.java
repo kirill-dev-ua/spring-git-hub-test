000997d00fc83a5040483b493f0b48cd26d2af08
Kirill Kuznetsov <kirillkuznetsov181@gmail.com>
2025-11-16 18:45:14 +0200

    start GitHub

---
+++ src/main/java/com/spring/test/SpringGitHubTestApplication.java
@@ -0,0 +1,12 @@
+package com.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitHubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitHubTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/test/demo/SpringGitHubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitHubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
